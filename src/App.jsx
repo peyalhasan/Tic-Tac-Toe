204d4277ff2b6c7268feb332e05fd4861eba927f
Peyal Hasan <peyalhasan143@gmail.com>
2025-11-06 01:54:06 +0600

    Winner showing successfully

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,19 @@ export function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [xIsNext, setXIsNext] = useState(true);
 
+  const winner = claculateWinner(squares);
 
+  let status;
+  if(winner){
+    status = `Winner: ${winner} `;
+  }
+  else{
+    status = "Next Player" + (xIsNext ? "X" : "O")
+  }
 
   function handleClick(i) {
-    if(squares[i]){
-      return;
+    if(squares[i] || claculateWinner(squares) ){
+      return ;
     }
     const nextSquares = squares.slice()
     if(xIsNext){
@@ -42,6 +50,9 @@ export function Board() {
 
   return (
     <>
+      <div>
+        {status}
+      </div>
       <div className="flex" >
         <Square value={squares[0]} onSquareClick={ () => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={ () => handleClick(1)} />
@@ -59,4 +70,24 @@ export function Board() {
       </div>
     </>
   )
+}
+
+function claculateWinner(squares){
+  const lines=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  for(let i = 0; i<lines.length; i++){
+    const [ a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ){
+      return squares[a];
+    }
+  }
+  return null;
 }
