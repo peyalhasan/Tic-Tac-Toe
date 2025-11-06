16e0c8e703c24480d91d34483ff1a0f7af49e824
Peyal Hasan <peyalhasan143@gmail.com>
2025-11-06 16:33:34 +0600

    Displaying History

--- src/App.jsx
+++ src/App.jsx
@@ -17,75 +17,112 @@ function Square({ value, onSquareClick }) {
   )
 }
 
-export function Board() {
+export function Board({squares, xIsNext, onPlay}) {
 
-  // for changing array value
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setXIsNext] = useState(true);
 
-  const winner = claculateWinner(squares);
+  const winner = calculateWinner(squares);
 
   let status;
-  if(winner){
+  if (winner) {
     status = `Winner: ${winner} `;
   }
-  else{
+  else {
     status = "Next Player" + (xIsNext ? "X" : "O")
   }
 
   function handleClick(i) {
-    if(squares[i] || claculateWinner(squares) ){
-      return ;
+    if (squares[i] || calculateWinner(squares)) {
+      return;
     }
     const nextSquares = squares.slice()
-    if(xIsNext){
+    if (xIsNext) {
       nextSquares[i] = "X"
     }
-    else{
+    else {
       nextSquares[i] = "O"
     }
-    setSquares(nextSquares)
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares)
   }
 
   return (
     <>
-      <div>
+      <div className="text-white">
         {status}
       </div>
-      <div className="flex" >
-        <Square value={squares[0]} onSquareClick={ () => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={ () => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={ () => handleClick(2)} />
+      <div className="flex text-black" >
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <div className="flex">
-        <Square value={squares[3]} onSquareClick={ () => handleClick(3)} />
-        <Square value={squares[4]}  onSquareClick={ () => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={ () => handleClick(5)} />
+      <div className="flex text-black">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <div className="flex">
-        <Square value={squares[6]} onSquareClick={ () => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={ () => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={ () => handleClick(8)} />
+      <div className="flex text-black">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   )
 }
 
-function claculateWinner(squares){
-  const lines=[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+
+export default function Game() {
+  
+  // for changing array value
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const [xIsNext, setXIsNext] = useState(true);
+  
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares){
+    setXIsNext(!xIsNext);
+    setHistory([...history, nextSquares])
+  }
+  const moves = history.map((squares, move) =>{
+    let description;
+    if(move > 0){
+      description = `Go to the move # ${move}`
+    }
+    else{
+      description = `Go to start the game`;
+    }
+    return(
+      <ol>
+        <button>{description}</button>
+      </ol>
+    )
+  })
+
+  return (
+    <div className="flex flex-col md:flex-row justify-center items-center gap-2 text-white">
+      <div>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} ></Board>
+      </div>
+      <div className=" bg-gray-950 p-5">
+        {moves}
+      </div>
+    </div>
+  )
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6], 
   ]
-  for(let i = 0; i<lines.length; i++){
-    const [ a, b, c] = lines[i];
-    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ){
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Board } from './App'
+import Game, { Board } from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Board></Board>
+    <Game></Game>
   </StrictMode>,
 )
